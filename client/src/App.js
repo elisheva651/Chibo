ac90d5a6fbeac6f874b17ca4c29fea8204c8a1b1
elisheva <eli65193@gmail.com>
2024-10-15 14:21:29 +0300

    init project

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h10>
+        Chibo
+      </h10>
+    </div>
+  );
+}
+
+export default App;
