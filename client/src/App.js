d48737868b43e1300b869efa97c9de25985107c6
elisheva <eli65193@gmail.com>
2024-10-18 13:52:27 +0300

    main page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Main from './components/Main';
+ 
 
 function App() {
   return (
-    <div className="App">
-      <h10>
-        Chibo
-      </h10>
+    <div className='App'>
+      <Main>
+
+      </Main>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,8 +1,27 @@
 const express = require('express')
-const app = express()
+const cors = require('cors');
+const app = express();
 
-app.get("/api",  (req, res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree"]})
-})
+const PORT = 5000
 
-app.listen(5000, () => {console.log("Server started on port 5000")})
+app.use(cors());
+app.use(express.json()); 
+
+
+const categories = ['יסודות', 'נקודות דיקור', 'צמחים', 'רפואה מערבית']
+const ways_to_learn =['הרצאות', 'סיכומים', 'משחקים', 'תרגילים'];
+// const categories = ['lectures'];
+
+app.get('/api/ways_to_learn', (req, res) => {
+    res.json(ways_to_learn);
+    return res
+  });
+
+
+app.get('/api/categories', (req, res) => {
+  res.json(categories);
+  return res
+});
+
+
+app.listen(PORT, () => {console.log(`Server started on port ${PORT}`)})
