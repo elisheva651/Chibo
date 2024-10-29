755f2295606cdc23371cf92319ef928a97250302
elisheva <eli65193@gmail.com>
2024-10-29 15:45:56 +0200

    trans to next

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,13 @@
 // pages/_app.js
 import '../styles/globals.css'; // Import global CSS
+import Home from './Home';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className='App'>
+      <Home/>
+    </div>
+  );
 }
 
 export default MyApp;
