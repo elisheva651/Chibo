0733fb3083516227d78b5d78113d540086d8ef24
elisheva <eli65193@gmail.com>
2024-12-11 11:03:00 +0200

    add meta

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,18 @@
 // pages/_app.js
 import '../styles/globals.css'; // Import global CSS
 import '../styles/memoryGame.css';
+import Head from 'next/head'; // Import the Head component
 
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
-
+    return <>
+    <Head>
+      {/* Add the viewport meta tag */}
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>My Game</title> {/* Optionally set the title of the page */}
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp;
